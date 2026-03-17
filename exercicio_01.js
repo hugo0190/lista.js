c8015f4954ec65c34257c155cfd800b355d8f33f
hugo0190 <hugo_gramme@estudante.sesisenai.org.br>
2026-03-17 08:44:44 -0300

    add- exercicio 1 finalizado

---
+++ exercicio_01.js
@@ -0,0 +1,31 @@
+// Função para somar os números fornecidos pelo usuário
+function somarNumeros() {
+    let soma = 0;
+    let continuar = true;
+
+    // Estou usando let para trabalhar valores no bloco de função.
+
+    // Loop para solicitar os números até que o usuário decida parar
+    while (continuar) {
+        let numero = parseFloat(prompt("Digite um número (ou digite '0' para encerrar):"));
+
+        // Verifica se o usuário digitou um número válido
+        // isNaN utilizamos para saber se o número está vazio.
+        if (!isNaN(numero)) {
+            // fique atento para !
+            soma += numero;
+        } else {
+            alert("Por favor, digite um número válido.");
+        }
+
+        // Pergunta ao usuário se deseja continuar
+        continuar = confirm("Deseja adicionar mais números?");
+    }
+
+    // Exibe o resultado da soma
+    alert("A soma dos números é: " + soma);
+    // de uma olhadinha na tag alert
+}
+
+// Chama a função para iniciar o processo de soma
+somarNumeros();
