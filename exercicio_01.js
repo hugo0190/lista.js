1f1dff319bb41fce07a1f4bca1c0147f61c8daf3
hugo0190 <hugo_gramme@estudante.sesisenai.org.br>
2026-03-19 08:03:59 -0300

    add- finalizei o exercício 2,3,4,5,6,7,8,9,10,11

--- exercicio_01.js
+++ exercicio_01.js
@@ -1,31 +1,24 @@
-// Função para somar os números fornecidos pelo usuário
-function somarNumeros() {
+function somarNumeros(){
     let soma = 0;
     let continuar = true;
 
-    // Estou usando let para trabalhar valores no bloco de função.
+    while(continuar) {
 
-    // Loop para solicitar os números até que o usuário decida parar
-    while (continuar) {
-        let numero = parseFloat(prompt("Digite um número (ou digite '0' para encerrar):"));
+        let numero = parseFloat(prompt("Digite um número ou digite '0' para encerrar: "));
 
-        // Verifica se o usuário digitou um número válido
-        // isNaN utilizamos para saber se o número está vazio.
-        if (!isNaN(numero)) {
-            // fique atento para !
-            soma += numero;
-        } else {
+        if(isNaN(numero)){
             alert("Por favor, digite um número válido.");
-        }
+        } else {
 
-        // Pergunta ao usuário se deseja continuar
-        continuar = confirm("Deseja adicionar mais números?");
-    }
+            soma += numero;
 
-    // Exibe o resultado da soma
+        };
+
+        continuar = confirm("Deseja adicionar mais número?");
+
+    };
     alert("A soma dos números é: " + soma);
-    // de uma olhadinha na tag alert
-}
 
-// Chama a função para iniciar o processo de soma
-somarNumeros();
+};
+
+//somarNumeros()
---
+++ exercicio_03.js
@@ -0,0 +1,32 @@
+function balancoGeral() {
+
+    let ganho = [];
+    let gasto = [];
+    let ganhoAno = 0;
+    let gastoAno = 0;
+    let total = 0;
+
+    for (let i = 0; i < 12; i++) {
+
+        ganho[i] = Number(prompt("Informe os ganhos do mês " + (i+1)));
+        gasto[i] = Number(prompt("Informe os gastos do mês " + (i+1)));
+
+        ganhoAno += ganho[i]; 
+        gastoAno += gasto[i];
+        
+    };
+
+    total = ganhoAno - gastoAno;
+
+
+
+    if(total > 0) {
+        alert("Lucro: " + total);
+    } else {
+        alert("Prejúizo: " + total);
+    };
+
+
+};
+
+//balancoGeral()
